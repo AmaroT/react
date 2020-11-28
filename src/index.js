f8120dc85216a5dda072309642be5064ad4763ee
Amaro Terrazas <42896111+AmaroT@users.noreply.github.com>
2020-11-27 23:06:51 -0600

    Event Basics

--- src/index.js
+++ src/index.js
@@ -29,20 +29,30 @@ function BookList() {
     return (
         <section className="bookList">
             {books.map((book, index) => {
-                return <Book key={book.id} book={book}></Book>;
+                return <Book key={book.id} {...book}></Book>;
             })}
         </section>
     );
 }
 
-const Book = (props) => {
-    console.log(props);
-    const {img, title, author} = props.book;
+const Book = ({img, title, author}) => {
+    const clickHandler = (e) => {
+        console.log(e);
+        console.log(e.target);
+        alert('hello world');
+    };
+    const complexExample = (author) =>{
+        console.log(author);
+    }
     return (
-        <article className="book">
+        <article className="book" onMouseOver={()=>{
+            console.log(title);
+        }}>
             <img src={img} alt=''/>
-            <h1>{title}</h1>
+            <h1 onClick={() => console.log(title)}>{title}</h1>
             <h4>{author}</h4>
+            <button type="button" onClick={clickHandler}>Reference Example</button>
+                <button type ="button" onClick={() => complexExample(author)}>more complex example</button>
         </article>
     );
 };
